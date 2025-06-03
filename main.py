eadb75a0f241ac63979e69f17598e94b5f8923a0
vilebile17 <somaurooibrahim@gmail.com>
2025-06-03 20:25:57 +0100

    Balls are now spawning

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 import pygame
 from constants import *
+from player import Player
+from asteroid import *
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
+
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable, )
+    asteroid_field = AsteroidField()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    time = pygame.time.Clock()
+    dt = 0
+
+    print(asteroid_field in updatable)
+
+    # GAME LOOP
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
         screen.fill("black")
+        for thing in updatable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
+        dt = time.tick(FPS) / 1000
 
 
 if __name__ == "__main__":
